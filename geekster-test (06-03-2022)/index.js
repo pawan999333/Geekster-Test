a2b930648540f1e9cdf0f2b232758316bba5477c
pawan kumar sharma <pawan1995219@gmail.com>
2022-03-06 15:05:59 +0530

    Add files via upload

---
+++ geekster-test (06-03-2022)/index.js
@@ -0,0 +1,46 @@
+function Details(id) {
+    appdetails(id);
+  }
+  
+  function appdetails(id) {
+    if (id == "1") {
+    let Apple = document.getElementById("id1");
+    Apple.innerHTML =
+      "<h2>iPhone 13 Pro Max</h2><br><p>₹126,003 - ₹174,503</p><br><p>Lightning-fast A15 Bionic chip for super charged productivity</p><br><p>The best battery life ever in an iPhone, upto 28 hours of video playback</p><br><p>17cm(6.7-inch) Super Retina XDR display with Pro</p>";
+  }
+   
+    if (id == "2") {
+        let samsung = document.getElementById("id2");
+        samsung.innerHTML =
+        "<h2>Samsung Galaxy M12</h2><br><p>₹12,003 - ₹15,503</p><br><p>13.7 cm (5.4-inch) Super Retina XDR display1</p><br><p>Cinematic mode adds shallow depth of field and shifts focus automatically in your videos</p><br><p>Advanced dual-camera system with 12MP Wide and Ultra Wide cameras; Photographic Styles, Smart HDR</p>";
+    }
+    if (id == "3") {
+      let realme = document.getElementById("id3");
+      realme.innerHTML =
+      "<h2>Realme C25s</h2><br><p>₹10,999 - ₹15,999</p><br><p>Cinematic mode adds shallow depth of field and shifts focus automatically in your videos</p><br><p>Advanced dual-camera system with 12MP Wide and Ultra Wide cameras; Photographic Styles, Smart HDR</p><br><p>Advanced dual-camera system with 12MP Wide and Ultra Wide cameras; Photographic Styles, Smart HDR</p>";
+    }
+
+    if (id == "4") {
+        let samsung1 = document.getElementById("id4");
+        samsung1.innerHTML =
+        "<h2>Samsung Galaxy M21</h2><br><p>₹22,003 - ₹55,503</p><br><p>16.21cm (6.4-inch) Super Retina XDR display1</p><br><p>Cinematic mode adds shallow depth of field and shifts focus automatically in your videos</p><br><p>Water resistant to a depth of 6 meters for up to 30 minutes</p>";
+    }
+
+    if (id == "5") {
+        let iPhone = document.getElementById("id5");
+        iPhone.innerHTML =
+        "<h2>Apple iPhone13 pro</h2><br><p>₹126,003 - ₹174,503</p><br><p>7MP TrueDepth Camera.</p><br><p>Talk time (wireless): Up to 25 hours; Internet use: Up to 15 hours; Video playback (wireless): Up to 16 hours</p><br><p>All-glass and aluminum design, water and dust resistant (rated IP67)</p>";
+    }
+
+    if (id == "6") {
+        let samsung2 = document.getElementById("id6");
+        samsung2.innerHTML =
+        "<h2>Samsung Gallexy F12</h2><br><p>₹26,999 - ₹74,503</p><br><p>7MP TrueDepth Camera.</p><br><p>Talk time (wireless): Up to 25 hours; Internet use: Up to 15 hours; Video playback (wireless): Up to 16 hours</p><br><p>Water resistant to a depth of 6 meters for up to 30 minutes</p>";
+    }
+
+    if (id == "6") {
+        let samsung3 = document.getElementById("id4");
+        samsung.innerHTML =
+        "<h2>Samsung Gallexy M32</h2><br><p>₹66,999 - ₹99,999</p><br><p> </p><br><p>Talk time (wireless): Up to 25 hours; Internet use: Up to 15 hours; Video playback (wireless): Up to 16 hours</p><br><p>All-glass and aluminum design, water and dust resistant (rated IP67)</p>";
+    }
+  }
